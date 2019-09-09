f00fe685ecf30821b63d7335baca6cf6d517193e
Victor <victor18ram@outlook.es>
2019-09-09 12:03:18 -0600

    verificar email

--- backend/registro_usuario.php
+++ backend/registro_usuario.php
@@ -37,21 +37,38 @@ function registro(string $username1, string $pwd1, string $codigo_conf1, int $ti
     $db = new Db();
     $conn = $db->getConn();
 
-    $insertar = $conn->prepare("INSERT INTO usuarios (email, pw, codigo_confirmacion, id_tipo_usuario, estatus) VALUES  ( ?, ?, ?, ?, ?)");
-    $insertar->bind_param("sssis", $username1, $pwd1, $codigo_conf1, $tipoUsuario1, $estatus1);
+    $resultado_email = validarEmailAddress($username1);
     
-    $insertar->execute();
+    if ($resultado_email==1){
+        $insertar = $conn->prepare("INSERT INTO usuarios (email, pw, codigo_confirmacion, id_tipo_usuario, estatus) VALUES  ( ?, ?, ?, ?, ?)");
+        $insertar->bind_param("sssis", $username1, $pwd1, $codigo_conf1, $tipoUsuario1, $estatus1);
+        $insertar->execute();
 
-    $r = $db->readResult($insertar->get_result());
-
-    if (empty($r)) {
+        //Agregar condicion de que se haga la insersion
+        if (empty($r)) {
+            $err = new ErrorResult("Error de registro", 401);
+            $output = $err;
+        } else {
+            $output = new SuccessResult("Registro correcto", true);
+            $output = $output;
+        }
+    }
+    else{
         $err = new ErrorResult("Error de registro", 401);
         $output = $err;
-    } else {
-        $output = new SuccessResult("Registro correcto", true);
-        $output = $output;
     }
 
     return $output;
+}
+
+function validarEmailAddress($email)
+{
+   // Regresa 1 si la cadena hace match
+
+   // Regresa 0 si no hace match
+
+   // Regresa FALSE si hubo un error 
+
+   return preg_match("/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})+$/", $email);
 }
 ?>
