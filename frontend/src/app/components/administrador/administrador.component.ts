9e882a3bcafaf5052832551fb9851e26518bce05
Gilberto Gastelum <gilberto.gastelum.acosta@gmail.com>
2019-10-09 12:52:35 -0600

    Datatable Implementada

--- frontend/src/app/components/administrador/administrador.component.ts
+++ frontend/src/app/components/administrador/administrador.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild,ViewChildren, QueryList } from '@angular/core';
 import { Area, Cat_empresa } from 'src/app/models/admin';
 import Swal from 'sweetalert2';
 import { AdminService } from '../../services/admin.service';
@@ -34,64 +34,94 @@ export class AdministradorComponent implements OnInit {
   estado3 = false; 
   estado4 = false; 
   estadoimagen = true;
-
-  
-
-  displayedColumns: string[] = ['id_area_estudio', 'nombre', 'estatus'];
-  dataSource = new MatTableDataSource<any>();
-  
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;  
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
-
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
- 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-  
-  
-  // Esto es para agregar algo a la lista y se puede usar para agregar cosas a la base de datos
- 
-  
   btnAgregarArea  : boolean;
   btnAgregarCategoria : boolean;
+
+  ColumnasCategorias: string[]=['id_tipo_empresa','nombre_empresa','estatus'];
+  ColumnasAreas: string[] = ['id_area_estudio', 'nombre', 'estatus'];
+  dataSource_AreasEstudio = new MatTableDataSource<any>();
+  dataSource_Categorias= new MatTableDataSource<any>();
+ 
+
+  applyFilterAreas(filterValue: string) {
+    this.dataSource_AreasEstudio.filter = filterValue.trim().toLowerCase();
+  }
+  applyFilterCategorias(filterValue: string) {
+    this.dataSource_Categorias.filter = filterValue.trim().toLowerCase();
+  }
+
+  private paginator: MatPaginator;
+  private sort: MatSort;
+  private paginator1: MatPaginator;
+  private sort1: MatSort;
+
+  
+  @ViewChild(MatSort,{static:false}) set matSort(ms: MatSort) {
+    this.sort = ms;
+
+    this.setDataSourceAreasAttributes();
+  }
+
+  @ViewChild(MatPaginator,{static:false}) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+
+    this.setDataSourceAreasAttributes();
+  }
+
+  setDataSourceAreasAttributes() {
+    
+    this.dataSource_AreasEstudio.paginator = this.paginator;
+    this.dataSource_AreasEstudio.sort = this.sort;
+  }
+  @ViewChild(MatPaginator,{static:false}) set matPaginator2 (mp2:MatPaginator){
+    this.paginator1=mp2;
+    this.setDataSourceCategoriasAttributes();
+  }
+  @ViewChild(MatSort,{static:false}) set matSort2 (ms2:MatSort){
+    this.sort1=ms2;
+    this.setDataSourceCategoriasAttributes();
+  }
+  setDataSourceCategoriasAttributes(){
+    this.dataSource_Categorias.paginator=this.paginator1;
+    this.dataSource_Categorias.sort=this.sort1;
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource_AreasEstudio.paginator=this.paginator;
+    this.dataSource_AreasEstudio.sort=this.sort;
+    this.dataSource_Categorias.paginator=this.paginator1;
+    this.dataSource_Categorias.sort=this.sort1;
+  }
+
   
   constructor(private adminservice:AdminService) { }
-  
   MostrarSolicitudes(){
     this.adminservice.get_solicitudes()
     .subscribe((response) => {
       if (response.success)
       {
         this.datos_solicitud = response.data;
-  
       }
       else {
         Swal.fire("Error", response.message, 'error');
       }
     });
   }
+
   MostrarAreas(){
     this.adminservice.get_areas()
     .subscribe((response) => {
       if (response.success)
       {
         this.datosarea = response.data;
-        this.dataSource.data=this.datosarea;
-     
+        this.dataSource_AreasEstudio.data=this.datosarea;
 
       }
       else {
         Swal.fire("Error", response.message, 'error');
       }
-      
     });
   }
-
-  
   
   MostrarCategorias(){
     this.adminservice.get_categorias()
@@ -99,7 +129,9 @@ export class AdministradorComponent implements OnInit {
       if (response.success)
       {
         this.datoscategoria = response.data;
-        // this .datosarea= this.areas;
+        this.dataSource_Categorias.data=this.datoscategoria;
+
+
       }
       else {
         Swal.fire("Error", response.message, 'error');
@@ -107,19 +139,13 @@ export class AdministradorComponent implements OnInit {
     });
   }
 
-  
-  ngOnInit() {
-    
+  ngOnInit() {    
     this.MostrarAreas();
     this.MostrarCategorias();
     this.MostrarSolicitudes();
-    
-    
   }
 
-  
   add_areaEstudio() {
-    this.dataSource.paginator = this.paginator;
     const nombre = $('#area').val();
     if (nombre ==='') {
       Swal.fire("No ingreso ningun valor");
@@ -135,16 +161,13 @@ export class AdministradorComponent implements OnInit {
           }
           else {
             Swal.fire("Error", response.message, 'error');
-          }
-    
+          }    
           this.btnAgregarArea = false;
-          
          $('#area').val('');
         });
-        
-   }
-
+      }
   }
+
   add_CategoriaEmpresa(){
     const nombre=$('#categoria').val();
     if (nombre===''){
@@ -162,36 +185,16 @@ export class AdministradorComponent implements OnInit {
         else{
           Swal.fire("Error", response.message, 'error');
         }
-
         this.btnAgregarCategoria=false;
-        
         $('#categoria').val('');
       });
     }
-    
-
   }
+
   eliminar(i) {
     this.datoscategoria.splice(i, 1);
   }
-  // agregar(){
-  //   this.btnagregarCliked=true;
-  //   this.adminservice.add_area(this.nuevaArea)
-  //   .subscribe((response) => {
-  //     if (response.success)
-  //     {
-  //       Swal.fire("correcto", response.message, 'success');
-  //       $("#categoria").val('');
-  //     }
-  //     else {
-  //       Swal.fire("Error", response.message, 'error');
-  //     }
-
-  //     this.btnagregarCliked = false;
-  //   });
-  //  }
-
-
+  
   categorias(numero) {
         // Este es para el primer boton 
         if (numero === 1) {
@@ -200,13 +203,13 @@ export class AdministradorComponent implements OnInit {
               this.estado = false;
               $('#categoriaboton').css('border-width', '1px');
 
+
             } else {
               this.estadoimagen = false;
               this.estado = true;
               this.estado2 = false;
               this.estado3 = false;
               this.estado4 = false;
-              this.funcioncolores1();
             }
       // Este es para el segundo boton 
         } else if (numero === 2) {
@@ -214,7 +217,6 @@ export class AdministradorComponent implements OnInit {
               this.estado2 = false;
               this.estadoimagen = true;
               $('#areas').css('border-width', '1px');
-
             } else {
               this.estado2 = true;
               this.estado = false;
@@ -229,7 +231,6 @@ export class AdministradorComponent implements OnInit {
               this.estado3 = false;
               this.estadoimagen = true; 
               $('#subareas').css('border-width', '1px');
-
             } else {
               this.estado3 = true;
               this.estadoimagen = false;
