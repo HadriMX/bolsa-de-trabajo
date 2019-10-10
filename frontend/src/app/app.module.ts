64f5681f5f820088737518220b11d03d89cc4b00
Adrian R <beltran17_98@hotmail.com>
2019-10-10 12:06:09 -0600

    Aplicado formato

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,20 +13,20 @@ import { AdministradorComponent } from './components/administrador/administrador
 import { EditarusuarioComponent } from './components/editarusuario/editarusuario.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
-import { AuthGuardService as AuthGuard }  from './auth/auth.guard';
+import { AuthGuardService as AuthGuard } from './auth/auth.guard';
 import { LoginGuardService as LoginGuard } from './auth/login.guard';
 import { AuthInterceptorService as AuthInterceptor } from './services/auth-interceptor.service';
 import * as $ from 'jquery';
 
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
-  {path: 'menu', component: MenuComponent, canActivate: [AuthGuard]},
-  {path: 'postulaciones', component: PostulacionesComponent, canActivate: [AuthGuard]},
-  {path: 'editarusuario', component: EditarusuarioComponent, canActivate: [AuthGuard]},
-  {path: 'administracion', component: AdministradorComponent, canActivate: [AuthGuard]},
-  {path: 'vacantes', component: VacantesComponent, canActivate: [AuthGuard]},
-  { path: '**', component: LoginComponent, canActivate: [LoginGuard]}
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+  { path: 'menu', component: MenuComponent, canActivate: [AuthGuard] },
+  { path: 'postulaciones', component: PostulacionesComponent, canActivate: [AuthGuard] },
+  { path: 'editarusuario', component: EditarusuarioComponent, canActivate: [AuthGuard] },
+  { path: 'administracion', component: AdministradorComponent, canActivate: [AuthGuard] },
+  { path: 'vacantes', component: VacantesComponent, canActivate: [AuthGuard] },
+  { path: '**', component: LoginComponent, canActivate: [LoginGuard] }
 ];
 
 @NgModule({
