5f46526564381e322797beb13fd3234136f5af26
Adrian R <beltran17_98@hotmail.com>
2019-10-28 12:27:04 -0700

    Creado session_starter_admin

--- backend/src/core/Solicitud.php
+++ backend/src/core/Solicitud.php
@@ -1,16 +1,16 @@
 <?php
 
-class Solicitud{
-
-    public static function get_solicitudes(){
+class Solicitud
+{
+    public static function get_solicitudes()
+    {
         $db = new Db();
         $conn = $db->getConn();
-        
-        $stmt = $conn->prepare("SELECT  * FROM solicitudes");
-        
+
+        $stmt = $conn->prepare("SELECT * FROM solicitudes");
+
         $stmt->execute();
         $r = $db->readResult($stmt->get_result());
-        return new SuccessResult("",$r);
+        return new SuccessResult("", $r);
     }
-
 }
