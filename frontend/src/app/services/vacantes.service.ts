011d3c51807cb4ad0e10cf44e2c07566e8477a91
Adrian R <beltran17_98@hotmail.com>
2019-10-29 10:04:40 -0700

    Inyectar PHPSESSID en auth-interceptor

--- frontend/src/app/services/vacantes.service.ts
+++ frontend/src/app/services/vacantes.service.ts
@@ -4,9 +4,7 @@ import { Vacante } from 'src/app/models/vacantes';
 import { ApiResponse } from 'src/app/models/api_response';
 import { Observable } from 'rxjs';
 import { Busqueda } from '../models/busqueda';
-import { CurrentUserService } from './current-user.service';
 import { HttpOptionsService } from './http-options.service';
-import { Usuario } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +14,9 @@ export class VacantesService {
   private endpointUrl = 'http://localhost/bdt/php/src/vacante/vacante.php';
 
   constructor(private http: HttpClient,
-    private httpOptions: HttpOptionsService,
-    private currentUserService: CurrentUserService) { }
+    private httpOptions: HttpOptionsService) { }
 
   getVacantes(busqueda: Busqueda): Observable<ApiResponse<Vacante[]>> {
-    var url = this.currentUserService.agregarPhpsessidEnUrl(this.endpointUrl);
-    return this.http.post<ApiResponse<Vacante[]>>(url, busqueda, this.httpOptions);
+    return this.http.post<ApiResponse<Vacante[]>>(this.endpointUrl, busqueda, this.httpOptions);
   }
 }
