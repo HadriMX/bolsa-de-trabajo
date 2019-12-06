cc6e108774d1e28f88878febb99824ff1d867b2a
Adrian R <beltran17_98@hotmail.com>
2019-12-06 12:37:02 -0700

    registrar vacante diseño WIP

--- frontend/src/app/components/registrar-vacante/registrar-vacante.component.ts
+++ frontend/src/app/components/registrar-vacante/registrar-vacante.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import '@ckeditor/ckeditor5-build-classic/build/translations/es';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Swal from 'sweetalert2';
 import { Vacante } from 'src/app/models/vacantes';
+import { AreaService } from 'src/app/services/area.service';
+import { Area } from 'src/app/models/area';
 
 @Component({
   selector: 'app-registrar-vacante',
@@ -9,22 +13,33 @@ import { Vacante } from 'src/app/models/vacantes';
 })
 export class RegistrarVacanteComponent implements OnInit {
 
+  areas: Area[] = [];
   nuevaVacante = new Vacante();
 
   public Editor = ClassicEditor;
   public EditorConfig = {
+    language: 'es',
     removePlugins: ['Link', 'EasyImage', 'Image', 'ImageCaption', 'ImageStyle', 'ImageToolbar', 'ImageUpload'],
     toolbar: ['heading', '|', 'bold', 'italic', 'bulletedList', 'numberedList', 'blockQuote', '|', 'undo', 'redo'],
     placeholder: 'Descripción de la empresa, detalles acerca del puesto, requisitos, beneficios, información adicional...'
   }
 
-  constructor() { }
+  constructor(private areaService: AreaService) { }
 
   ngOnInit() {
+    this.getAreas();
   }
 
-  test() {
-    console.log(this.nuevaVacante.descripcion_puesto);
+  getAreas() {
+    this.areaService.get_areasMenu()
+      .subscribe((response) => {
+        if (response.success) {
+          this.areas = response.data;
+        }
+        else {
+          Swal.fire("Error", response.message, 'error');
+        }
+      });
   }
 
 }
