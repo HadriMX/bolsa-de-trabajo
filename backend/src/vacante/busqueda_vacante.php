29cc743378d30ec10b7f49226b1c26578cf7d4bb
Luis Arredondo G <47874043+b0sque13@users.noreply.github.com>
2020-01-27 12:09:14 -0700

    php funciones aspirantes

--- backend/src/vacante/busqueda_vacante.php
+++ backend/src/vacante/busqueda_vacante.php
@@ -91,7 +91,7 @@ function getVacantes($titulo,$ubicacion,$idsueldo,$idfecha,$idarea)
     }
 
     $id_usuario = $_SESSION["currentUser"]["id_usuario"];
-    $stmt = $conn->prepare("SELECT * FROM vacantes_activos WHERE (((titulo_vacante LIKE ?) or (nombre_empresa LIKE ?)) or ?) AND ((entidad_federativa LIKE ?) or ?) AND ((sueldo >= ? AND sueldo <= ?) or ?) AND ((fecha_publicacion >= (NOW() - INTERVAL ? DAY)) or ? ) AND ((area_estudio LIKE ?) or ? ) AND (id_vacante NOT IN (SELECT id_vacante FROM postulados WHERE id_candidato = ?))");
+    $stmt = $conn->prepare("SELECT * FROM vacantes_activos WHERE (((titulo_vacante LIKE ?) or (nombre_empresa LIKE ?)) or ?) AND ((entidad_federativa LIKE ?) or ?) AND ((sueldo >= ? AND sueldo <= ?) or ?) AND ((fecha_publicacion >= (NOW() - INTERVAL ? DAY)) or ? ) AND ((area_estudio LIKE ?) or ? ) AND (id_vacante NOT IN (SELECT id_vacante FROM postulados WHERE id_candidato = ?)) ORDER BY fecha_publicacion DESC");
     $titulo = "%".$titulo."%";
     $ubicacion = "%".$ubicacion."%";
     $nombreArea = "%".$nombreArea."%";
