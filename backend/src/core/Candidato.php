2d5bfc57274884a3e46f3c8c5a824901e2c545a3
Luis Arredondo G <47874043+b0sque13@users.noreply.github.com>
2020-01-31 10:15:02 -0700

    php actualizar info usuario

--- backend/src/core/Candidato.php
+++ backend/src/core/Candidato.php
@@ -4,53 +4,67 @@ class Candidato
 {
     public static function update(array $candidato)
     {
+        $bandera = 0; //bandera 0 = datos completos, 1 = datos incompletos
         $db = new Db();
         $conn = $db->getConn();
-        $stmt = $conn->prepare("REPLACE INTO candidatos(id_usuario, nombre, apellido1, apellido2, genero, fecha_nacimiento, calle, colonia, cp, ciudad, id_municipio, id_entidad_federativa, id_grado_estudios, id_area_estudio, id_subarea_estudio, ruta_cv, telefono) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+        $stmt = $conn->prepare("REPLACE INTO candidatos(id_usuario, nombre, apellido1, apellido2, fecha_nacimiento, genero, telefono, id_entidad_federativa, id_municipio, ciudad, colonia, cp, calle, num_ext, id_grado_estudios, id_area_estudio, escuela, ruta_curp, ruta_id, ruta_cv) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
         
+        //info personal
         $id_usuario = $candidato['id_usuario'];
         $nombre = $candidato['nombre'];
         $apellido1 = $candidato['apellido1'];
         $apellido2 = $candidato['apellido2'];
-        $genero = $candidato['genero'];
         $fecha_nacimiento = $candidato['fecha_nacimiento'];
-        $calle = $candidato['calle'];
-        $colonia = $candidato['colonia'];
-        $cp = $candidato['cp'];
-        $ciudad = $candidato['ciudad'];
-        $id_municipio = $candidato['id_municipio'];
+        $genero = $candidato['genero'];
+        $telefono = trim($candidato['telefono']);
+        
+        //direccion
         $id_entidad_federativa = $candidato['id_entidad_federativa'];
+        $id_municipio = $candidato['id_municipio'];
+        $ciudad = trim($candidato['ciudad']);
+        $colonia = trim($candidato['colonia']);
+        $cp = $candidato['cp'];
+        $calle = trim($candidato['calle']);
+        $num_ext = $candidato['num_ext'];
+        
+        //info academica
         $id_grado_estudios = $candidato['id_grado_estudios'];
         $id_area_estudio = $candidato['id_area_estudio'];
-        $id_subarea_estudio = $candidato['id_subarea_estudio'];
+        $escuela = trim($candidato['escuela']);
+        $ruta_curp = $candidato['ruta_curp'];
+        $ruta_id = $candidato['ruta_id'];
         $ruta_cv = $candidato['ruta_cv'];
-        $telefono = $candidato['telefono'];
         
-        $stmt->bind_param('isssssssssiiiiiss',
-            $id_usuario,
-            $nombre,
-            $apellido1,
-            $apellido2,
-            $genero,
-            $fecha_nacimiento,
-            $calle,
-            $colonia,
-            $cp,
-            $ciudad,
-            $id_municipio,
-            $id_entidad_federativa,
-            $id_grado_estudios,
-            $id_area_estudio,
-            $id_subarea_estudio,
-            $ruta_cv,
-            $telefono);
+        $stmt->bind_param('issssssiisssssiissss',
+        $id_usuario,
+        $nombre,
+        $apellido1,
+        $apellido2,
+        $fecha_nacimiento,
+        $genero,
+        $telefono,
+        $id_entidad_federativa,
+        $id_municipio,
+        $ciudad,
+        $colonia,
+        $cp,
+        $calle,
+        $num_ext,
+        $id_grado_estudios,
+        $id_area_estudio,
+        $escuela,
+        $ruta_curp,
+        $ruta_id,
+        $ruta_cv);
+
+        //Falta validar datos aún
 
         $stmt->execute();
 
         if ($stmt->affected_rows > 0) {
-            $output = new SuccessResult("Update OK", true);
+            $output = new SuccessResult("Tu información ha sido guardada correctamente (Falta validar)", true);
         } else {
-            $output = new ErrorResult("No se pudo actualizar la base de datos.", 515);
+            $output = new ErrorResult("Error: No se pudo guardar la información. Intentelo mas tarde", 515);
         }
 
         $stmt->close();
